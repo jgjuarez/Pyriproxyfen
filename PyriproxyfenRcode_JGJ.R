64d9341908d5a4ae7ca92c993eb408c6eb8773a3
Jose G Juarez <jjuarezvaldez@gmail.com>
2021-03-05 15:53:02 -0600

    Upload of files March 5th

---
+++ PyriproxyfenRcode_JGJ.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(readxl)
+library(dplyr)
+library(tidyr)
+
+df1 <- read_excel("Pyriproxyfen_V1.1_JGJ_20210305.xlsx", 
+                                             sheet = "Pyro")
+
+
